8154e37da714cdf2afd382515f695c51466a6368
Niels Jaspers <naeliss.github@gmail.com>
2024-09-26 16:36:33 +0200

    JFrame added for swing test

---
+++ src/main/java/com/comicbookreader/Main.java
@@ -0,0 +1,22 @@
+package com.comicbookreader;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+    public static void main(String[] args) {
+
+        final int HEIGHT_FRAME = 400;
+        final int WIDTH_FRAME = 400;
+
+        JFrame frame = new JFrame("Comic Book Reader");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setSize(WIDTH_FRAME, HEIGHT_FRAME);
+
+        frame.setLayout(null);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
