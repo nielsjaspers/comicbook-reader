e8b85c4189563a35886fa60459cdca611eeb6a6f
Niels Jaspers <naeliss.github@gmail.com>
2024-09-26 16:37:36 +0200

    removed unused import statements

--- src/main/java/com/comicbookreader/Main.java
+++ src/main/java/com/comicbookreader/Main.java
@@ -1,8 +1,5 @@
 package com.comicbookreader;
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class Main {
     public static void main(String[] args) {
