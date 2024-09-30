2eebe43b301d999c9fbb168b120adc6cb0feb302
Niels Jaspers <naeliss.github@gmail.com>
2024-09-30 11:37:19 +0200

    correct frame scaling

--- src/main/java/com/comicbookreader/Main.java
+++ src/main/java/com/comicbookreader/Main.java
@@ -1,8 +1,12 @@
 package com.comicbookreader;
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
+
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 
         final int HEIGHT_FRAME = 400;
         final int WIDTH_FRAME = 400;
@@ -10,7 +14,9 @@ public class Main {
         JFrame frame = new JFrame("Comic Book Reader");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        frame.setSize(WIDTH_FRAME, HEIGHT_FRAME);
+        System.out.println("h = " + dim.height + "\nw = " + dim.width);
+
+        frame.setSize(dim.width, dim.height);
 
         frame.setLayout(null);
         frame.setLocationRelativeTo(null);
