71bcb814bb5884d0a968801fd3b9a6b84f7f1b70
MaxvDijk <maxingovd@gmail.com>
2024-10-02 21:33:25 +0200

    Made a basic ui with buttons and an indication on which page you are.
    Need to fix the possibility of going further then there are pages vice versa. need to implement arrow key usages.

--- src/main/java/com/comicbookreader/Main.java
+++ src/main/java/com/comicbookreader/Main.java
@@ -1,24 +1,48 @@
 package com.comicbookreader;
+import com.comicbookreader.comicbook.ComicbookreaderUI;
+import com.comicbookreader.comicbook.Page;
+import com.comicbookreader.comicbook.CBZParser;
 import com.comicbookreader.comicbook.Comicbook;
 
 import javax.swing.*;
 import java.awt.*;
 import java.io.*;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        CBZParser cbzParser = new CBZParser();
+        String cbzFilePath = "imported_comics/Deadpool Team-Up 002 (2024) (Digital) (Shan-Empire).cbz";  // Provide the path to your CBZ file
 
-        JFrame frame = new JFrame("Comic Book Reader");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ArrayList<Page> pages = new CBZParser().extractPages(cbzFilePath);
+        new ComicbookreaderUI(pages);
 
-        System.out.println("h = " + dim.height + "\nw = " + dim.width);
-
-        frame.setSize(dim.width, dim.height);
-
-        frame.setLayout(null);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+//        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+//
+//        JFrame frame = new JFrame("Comic Book Reader");
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//
+//
+//
+//        System.out.println("h = " + dim.height + "\nw = " + dim.width);
+//
+//        frame.setSize(dim.width, dim.height);
+//
+//        frame.setLocationRelativeTo(null);
+//        frame.setVisible(true);
+//
+//        ArrayList<Page> pages = (ArrayList<Page>) cbzParser.extractPages(cbzFilePath);
+//
+//        for (Page page : pages) {
+//            System.out.println("page.image = " + page.image);
+//            if (page.image != null) {
+//                JLabel label = new JLabel(new ImageIcon(page.image));
+//                frame.getContentPane().removeAll();  // Remove previous image
+//                frame.getContentPane().add(label, BorderLayout.CENTER);
+//                frame.revalidate();  // Revalidate the frame to display changes
+//                frame.repaint();
+//            }
+//        }
     }
 }
---
+++ src/main/java/com/comicbookreader/comicbook/ComicbookreaderUI.java
@@ -0,0 +1,85 @@
+package com.comicbookreader.comicbook;
+
+import com.comicbookreader.Main;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+public class ComicbookreaderUI implements ActionListener {
+    private JFrame frame;
+    private JLabel imageLabel;
+    private JLabel pageNumberLabel;
+    private ArrayList<Page> pages;
+    private int currentPage = 0;
+
+    public ComicbookreaderUI(ArrayList<Page> pages) {
+       this.pages = pages;
+       initUI();
+
+    }
+
+    public void initUI(){
+        frame = new JFrame("Comic Book Reader");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setSize(dim.width, dim.height);
+
+        frame.setLayout(new BorderLayout());
+
+        imageLabel = new JLabel();
+        imageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        frame.add(imageLabel, BorderLayout.CENTER);
+
+        JPanel navigationPanel = new JPanel(new FlowLayout());
+        JButton previousButton = new JButton("< Previous");
+        previousButton.addActionListener(this);
+        JButton nextButton = new JButton ("Next >");
+        nextButton.addActionListener(this);
+
+        pageNumberLabel = new JLabel("Current page: " + (currentPage + 1));
+
+        navigationPanel.add(previousButton);
+        navigationPanel.add(pageNumberLabel);
+        navigationPanel.add(nextButton);
+
+        frame.add(navigationPanel, BorderLayout.SOUTH);
+
+        frame.setVisible(true);
+        updatePageDisplay();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
+
+        if (command.equals("Next >")) {
+            currentPage ++;
+            updatePageDisplay();
+            System.out.println("The page is going up and = " + currentPage);
+
+
+        } else if (command.equals("< Previous")) {
+            currentPage --;
+            updatePageDisplay();
+            System.out.println("The page is going down and = " + currentPage);
+        }
+
+    }
+
+    private void updatePageDisplay(){
+        BufferedImage img = pages.get(currentPage).image;
+        Dimension frameSize = frame.getSize();
+        Image scaledImage = img.getScaledInstance(frameSize.width, frameSize.height, Image.SCALE_SMOOTH);
+
+        if (img != null){
+            imageLabel.setIcon(new ImageIcon(scaledImage));
+            pageNumberLabel.setText("Current page: " + (currentPage + 1));
+        }
+
+    }
+}
